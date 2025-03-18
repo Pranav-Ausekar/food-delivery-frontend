e7231b7fc85ec5b3ef66e0b7fd250ab83310972b
Pranav Ausekar <pranavausekar1@gmail.om>
2025-03-18 23:11:45 +0530

    backend url added from render

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -6,6 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
+    // const url = "http://localhost:3000"
     const url = "http://localhost:3000"
     const [token, setToken] = useState("")
 
